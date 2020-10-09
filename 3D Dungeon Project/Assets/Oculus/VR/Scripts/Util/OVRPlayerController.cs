954ff17b441e0eaa7716e8adc2b294b6c08d72f4
Nryder1102 <45008433+Nryder1102@users.noreply.github.com>
2020-10-09 13:17:29 -0400

    Backup
    Got VR and M/KB to work independently, need to combine somehow

--- 3D Dungeon Project/Assets/Oculus/VR/Scripts/Util/OVRPlayerController.cs
+++ 3D Dungeon Project/Assets/Oculus/VR/Scripts/Util/OVRPlayerController.cs
@@ -135,14 +135,14 @@ public class OVRPlayerController : MonoBehaviour
 	protected CharacterController Controller = null;
 	protected OVRCameraRig CameraRig = null;
 
-	private float MoveScale = 1.0f;
-	private Vector3 MoveThrottle = Vector3.zero;
+	public float MoveScale = 1.0f;
+	public Vector3 MoveThrottle = Vector3.zero;
 	private float FallSpeed = 0.0f;
 	private OVRPose? InitialPose;
 	public float InitialYRotation { get; private set; }
 	private float MoveScaleMultiplier = 1.0f;
 	private float RotationScaleMultiplier = 1.0f;
-	private bool SkipMouseRotation = true; // It is rare to want to use mouse movement in VR, so ignore the mouse by default.
+	public bool SkipMouseRotation = true; // It is rare to want to use mouse movement in VR, so ignore the mouse by default.
 	private bool HaltUpdateMovement = false;
 	private bool prevHatLeft = false;
 	private bool prevHatRight = false;
