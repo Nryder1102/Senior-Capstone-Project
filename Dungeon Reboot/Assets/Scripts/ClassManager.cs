40c70a27f2a3835f03f5961ca857d426c66d3d12
Nryder1102 <45008433+Nryder1102@users.noreply.github.com>
2021-01-21 12:24:25 -0500

    Backup

---
+++ Dungeon Reboot/Assets/Scripts/ClassManager.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassManager : MonoBehaviour
+{
+    //Variables
+    public int healthMax;
+    public int manaMax;
+    public int expMax;
+
+    //Stats
+    //1 Assignable Point per Level
+    public int level = 1;
+    //Dodge Chance + 5%/1 point(?) (From 5% base)
+    public int dex = 1;
+    //Attack Strength + 1/Point (Adds on to weapon attack)
+    public int str = 1;
+    //Health + 5/1 point(?) (Adds on to class base)
+    public int vit = 1;
+    //Mana + 5/1 point(?) (Adds on to clase base)
+    public int intel = 1;
+    //Makes negotiations more successfull + 5%/Point (From 5% base)
+    public int cha = 1;
+    //Increases Drop Chance, as well as give a 5% boost to Dodge and Negotiations per point (From 5% base)
+    public int luk = 1;   
+
+    //Bars
+    int health;
+    int mana;
+    int exp; 
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameManager.cconfirm2 == 2)
+        {
+            Stats();
+        }
+    }
+
+    
+    void Stats()
+    {
+       
+        if(GameManager.classnum == 1)
+        {
+            healthMax = 75;
+            health = 75;
+            manaMax = 30;
+            mana = 30;
+            expMax = 50;
+            exp = 0;
+            dex = dex + 2;
+            luk = luk + 2;
+            GameManager.cconfirm2 = 3;
+        }
+    
+        if(GameManager.classnum == 2)
+        {
+            healthMax = 50;
+            health = 50;
+            manaMax = 75;
+            mana = 75;
+            expMax = 50;
+            exp = 0;
+            intel = intel + 2;
+            luk = luk + 2;
+            GameManager.cconfirm2 = 3;
+        }
+
+        if(GameManager.classnum == 3)
+        {
+            healthMax = 125;
+            health = 125;
+            manaMax = 30;
+            mana = 30;
+            expMax = 50;
+            exp = 0;
+            vit = vit + 2;
+            cha = cha + 2;
+            GameManager.cconfirm2 = 3;
+        }
+
+        if(GameManager.classnum == 4)
+        {
+            healthMax = 75;
+            health = 75;
+            manaMax = 40;
+            mana = 40;
+            expMax = 50;
+            exp = 0;
+            dex = dex + 2;
+            cha = cha + 2;
+            GameManager.cconfirm2 = 3;
+        }
+
+        if(GameManager.classnum == 5)
+        {
+            healthMax = 100;
+            health = 100;
+            manaMax = 30;
+            mana = 30;
+            expMax = 50;
+            exp = 0;
+            vit = vit + 2;
+            str = str + 2;
+            GameManager.cconfirm2 = 3;
+        }
+
+        if(GameManager.classnum == 6)
+        {
+            healthMax = 100;
+            health = 100;
+            manaMax = 50;
+            mana = 50;
+            expMax = 50;
+            exp = 0;
+            str = str + 2;
+            cha = cha + 2;
+            GameManager.cconfirm2 = 3;
+        }
+    }
+}
+
+
+
---
+++ Dungeon Reboot/Assets/Scripts/GameManager.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Button roguebttn;
+    public Button magebttn;
+    public Button paladinbttn;
+    public Button rangerbttn;
+    public Button fighterbttn;
+    public Button beastbttn;
+    public Button cconfirm;
+    public static int classnum;
+    public static int cconfirm2;
+    public SpriteState beastdisabled;
+    public Sprite fighterdisabled;
+    public Sprite rangerdisabled;
+    public Sprite paladindisabled;
+    public Sprite magedisabled;
+    public Sprite roguedisabled;
+    public Sprite beastdetails;
+    public Sprite fighterdetails;
+    public Sprite rangerdetails;
+    public Sprite paladindetails;
+    public Sprite magedetails;
+    public Sprite roguedetails;
+
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cconfirm.interactable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ClassSelect1()
+    {
+        classnum = 1;
+        cconfirm2 = 1;
+        cconfirm.interactable = true;
+    }
+    public void ClassSelect2()
+    {
+        classnum = 2;
+        cconfirm2 = 1;
+        cconfirm.interactable = true;
+    }
+    public void ClassSelect3()
+    {
+        classnum = 3;
+        cconfirm2 = 1;
+        cconfirm.interactable = true;
+    }
+    public void ClassSelect4()
+    {
+        classnum = 4;
+        cconfirm2 =1;
+        cconfirm.interactable = true;
+    }
+    public void ClassSelect5()
+    {
+        classnum = 5;
+        cconfirm2 = 1;
+        cconfirm.interactable = true;
+    }
+    public void ClassSelect6()
+    {
+        classnum = 6;
+        cconfirm2 =1;
+        cconfirm.interactable = true;
+    }
+
+    public void ConfirmClass()
+    {
+        BttnDisable();
+
+    }
+    public void BttnDisable()
+    {
+       
+        if (classnum == 1 && cconfirm2 == 1)
+        {
+            magebttn.interactable = false;
+            paladinbttn.interactable = false;
+            rangerbttn.interactable = false;
+            fighterbttn.interactable = false;
+            beastbttn.interactable = false;
+            cconfirm2 = 2;
+        }
+        if (classnum == 2)
+        {
+            roguebttn.interactable = false;
+            paladinbttn.interactable = false;
+            rangerbttn.interactable = false;
+            fighterbttn.interactable = false;
+            beastbttn.interactable = false;
+            cconfirm2 = 2;
+        }
+        if (classnum == 3)
+        {
+            roguebttn.interactable = false;
+            magebttn.interactable = false;
+            rangerbttn.interactable = false;
+            fighterbttn.interactable = false;
+            beastbttn.interactable = false;
+            cconfirm2 = 2;
+        }
+        if (classnum == 4)
+        {
+            roguebttn.interactable = false;
+            magebttn.interactable = false;
+            paladinbttn.interactable = false;
+            fighterbttn.interactable = false;
+            beastbttn.interactable = false;
+            cconfirm2 = 2;
+        }
+        if (classnum == 5)
+        {
+            roguebttn.interactable = false;
+            magebttn.interactable = false;
+            paladinbttn.interactable = false;
+            rangerbttn.interactable = false;
+            beastbttn.interactable = false;
+            cconfirm2 = 2;
+        }
+        if (classnum == 6)
+        {
+            roguebttn.interactable = false;
+            magebttn.interactable = false;
+            paladinbttn.interactable = false;
+            rangerbttn.interactable = false;
+            fighterbttn.interactable = false;
+            cconfirm2 = 2;
+            
+        }
+
+    }
+
+}
